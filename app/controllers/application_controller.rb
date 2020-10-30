8d47bb00d5255b7f2bc3f19e9059ea703c28400d
Artur <amrozinski@gmail.com>
2020-10-30 18:07:36 +0000

    added functionality to users and articles based on log in status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+
+helper_method :current_user, :loggin_in? #this makes sure, that current use is available also
+#for ApplicationHelper, not only for ApplicationController
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def logged_in?
+    !!current_user # this returns a boolean value, and makes sure user is on
+  end
 end
