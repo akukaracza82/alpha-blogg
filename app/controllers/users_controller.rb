37858c49aa233f0ae5990b8d128dddaf53b7c832
Artur <amrozinski@gmail.com>
2020-11-04 14:12:33 +0000

    added destroy route to user controller and depended to user model to destroy all articles if user has been deleted

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_action :require_user, only: [:edit, :update]
   before_action :require_same_user, only: [:edit, :update]
 
@@ -38,6 +38,10 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page], per_page: 5)
   end
 
+  def destroy
+    @user.destroy
+    session[:user_id] = nil
+  end
   private
 
   def user_params
