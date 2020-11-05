3dfa74cdb3634fcdbf277efa50e882859774ed82
Artur <amrozinski@gmail.com>
2020-11-05 19:19:54 +0000

    added admin functionality. full set up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,12 +19,4 @@ helper_method :current_user, :logged_in? #this makes sure, that current use is a
       redirect_to login_path
     end
   end
-
-  def require_same_user
-    if current_user != @article.user
-      flash[:alert] = "You can only edit or delete your own articles"
-      redirect_to @article
-    end
-  end
-
 end
